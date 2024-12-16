196a0e44acf94880af9595a586e85185943b2f0c
Yessentemir <yessentemir.utepov@gmail.com>
2024-12-16 20:15:31 +0500

    new versiion

--- main.go
+++ main.go
@@ -2,35 +2,48 @@ package main
 
 import (
 	"log"
+	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
-	// Укажите ваш токен
+	// Specify your token
 	bot, err := tgbotapi.NewBotAPI("7758148248:AAGPFJYGWj-puL619sOq76sELDUy1wFKous")
 	if err != nil {
 		log.Panic(err)
 	}
 
-	bot.Debug = true // Включить режим отладки
-
+	bot.Debug = true
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	// Настройка обновлений
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
 	updates := bot.GetUpdatesChan(u)
 
-	// Обработка обновлений
 	for update := range updates {
-		if update.Message != nil { // Если это сообщение
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		if update.Message != nil {
+			chatID := update.Message.Chat.ID
+			text := update.Message.Text
 
-			// Ответ пользователю
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Привет, "+update.Message.From.FirstName+"!")
-			bot.Send(msg)
+			// Log received message
+			log.Printf("Received message from user %s: %s", update.Message.From.UserName, text)
+
+			if strings.HasPrefix(text, "/start") {
+				sendMessage(bot, chatID, "Welcome! You have started the bot.")
+			} else if strings.HasPrefix(text, "/help") {
+				sendMessage(bot, chatID, "Here are the commands you can use:\n/start - Start the bot\n/help - Show this help message")
+			} else {
+				sendMessage(bot, chatID, "I only understand commands for now. Try /start or /help.")
+			}
 		}
+	}
+}
+
+func sendMessage(bot *tgbotapi.BotAPI, chatID int64, text string) {
+	msg := tgbotapi.NewMessage(chatID, text)
+	if _, err := bot.Send(msg); err != nil {
+		log.Printf("Error sending message: %v", err)
 	}
 }
